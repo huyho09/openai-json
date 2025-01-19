242f41f6e3735ea3c3e3cbfc1a812503ed1f3cf3
Huy Ho <huynino0496@gmail.com>
2025-01-19 17:01:05 +0700

    Create dotenv to store OpenAI access token

--- openai-py/main.py
+++ openai-py/main.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 # Allow all CORS
 CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5500"}})
 
+#openai.api_key = os.getenv("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", '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')
 openai.api_key = os.getenv("OPENAI_API_KEY", 'OPENAI_API_KEY')
 
 # Initial data.json
